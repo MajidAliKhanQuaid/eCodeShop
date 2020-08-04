88262b78c5c3a4e90849f22042dd5a0e5499b96c
Majid Ali Khan Quaid <MajidAliKhanQuaid@outlook.com>
2020-08-04 12:33:42 +0500

    Update Production Env File

--- eCodeShop.Web/ecode-shop-angular/src/environments/environment.prod.ts
+++ eCodeShop.Web/ecode-shop-angular/src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: true
 };
 
-export const dummyUrl = 'https://reqres.in/api/users?page=2';
-export const apiUrl = 'http://makq.azurewebsites.net/api'; //44396';
+export const API_URL: string = 'http://makq.azurewebsites.net/api'; //44396';
+
+export const ACCESS_TOKEN: string = '';
+export const REFRESH_TOKEN: string = '';
